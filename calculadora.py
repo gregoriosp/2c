9138bb552767f6c1bdc280a2371e6cd0a2f33905
gregoriosp <nivaldogregorio.ufabc@professor.educacao.sp.gov.br>
2024-03-24 15:52:33 -0300

    python

---
+++ calculadora.py
@@ -0,0 +1,37 @@
+
+def adicao(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erro: divisão por zero"
+
+print("Selecione a operação desejada:")
+print("1 - Adição")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+
+escolha = input("Digite sua escolha (1/2/3/4): ")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+if escolha == '1':
+    print(num1, "+", num2, "=", adicao(num1, num2))
+elif escolha == '2':
+    print(num1, "-", num2, "=", subtracao(num1, num2))
+elif escolha == '3':
+    print(num1, "*", num2, "=", multiplicacao(num1, num2))
+elif escolha == '4':
+    print(num1, "/", num2, "=", divisao(num1, num2))
+else:
+    print("Opção inválida")
